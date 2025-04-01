2a1967d3267b2e67d78983ddecfcff4410b280e5
SaltyFurball54 <dnet4906@gmail.com>
2025-04-01 19:09:46 +0300

    Add project files.

---
+++ Project1/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project1.Entities;
+
+namespace Project1
+{
+    public class AppDbContext : DbContext
+    {
+        private string _connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=UN;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+
+
+    }
+}
---
+++ Project1/Migrations/20250327174837_InsertInto.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Project1.Migrations
+{
+    /// <inheritdoc />
+    public partial class InsertInto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+            INSERT INTO Students (Name,Description)
+            VALUES ('A1', 'text 1'),
+                   ('B2', 'text 2'),
+                   ('C3', 'text 3');
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
